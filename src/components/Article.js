bb94f2edb34781d89dd2ec48bc808224fbbb4280
VICTOR <victor.kipkoech@student.moringaschool.com>
2023-11-13 09:55:09 +0300

    completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,11 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      <p> please pass this test </p>
+
     </div>
   );
 }
 
 export default Article;
+
